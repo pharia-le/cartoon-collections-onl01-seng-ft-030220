1cdfebb2ef199caa2b2dc4076c6fbba2bb80c913
Pharia Le <pharia.le@gmail.com>
2020-03-05 06:58:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,10 @@ def roll_call_dwarves(array)
   array.each_with_index { |name, i| puts "#{i+1} #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |planet|
+    planet[0].upcase + planet[1..-1] + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
