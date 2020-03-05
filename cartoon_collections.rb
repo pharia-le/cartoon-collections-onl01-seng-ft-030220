c9faa5a6f7c9e928fc27bf886f14ff716391a0e6
Pharia Le <pharia.le@gmail.com>
2020-03-05 07:04:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(array)
   return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.find?
 end
